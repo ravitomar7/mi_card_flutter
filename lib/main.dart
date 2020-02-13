a58519c3f135a705d5b95f4cd51743b3d19751de
Ravi Tomar <ravitomar7@gmail.com>
2020-02-13 10:10:37 +0530

    Final Business Card App

--- lib/main.dart
+++ lib/main.dart
@@ -16,13 +16,49 @@ class MyApp extends StatelessWidget {
             CircleAvatar(
 //
               radius: 100.0,
-              backgroundImage: AssetImage('images/kittu.jpg'),
+              backgroundImage: AssetImage("images/kittu.jpg"),
             ),
-            Text("Kritika & Vedika",
-                style: TextStyle(
-                    fontSize: 40,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.white)),
+            Text(
+              "Kritika & Vedika",
+              style: TextStyle(
+                  fontSize: 40,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
+                  fontFamily: 'Pacifico'),
+            ),
+            Text(
+              "Cuteness Overloaded",
+              style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.teal.shade900,
+                  fontFamily: 'SourceSans',
+                  letterSpacing: 2.5),
+            ),
+            Container(
+              color: Colors.white,
+              padding: EdgeInsets.symmetric(horizontal: 10.0, vertical: 10.0),
+              margin: EdgeInsets.symmetric(horizontal: 10.0, vertical: 20),
+              child: Row(
+                children: <Widget>[
+                  Icon(Icons.phone),
+                  SizedBox(width: 40.0),
+                  Text("+91-123 456 789")
+                ],
+              ),
+            ),
+            Container(
+              color: Colors.white,
+              padding: EdgeInsets.symmetric(horizontal: 10.0, vertical: 10.0),
+              margin: EdgeInsets.symmetric(horizontal: 10.0, vertical: 20),
+              child: Row(
+                children: <Widget>[
+                  Icon(Icons.email),
+                  SizedBox(width: 40.0),
+                  Text("ravitomar7@gmail.com")
+                ],
+              ),
+            ),
           ],
         ),
       ),
